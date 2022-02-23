244ef9eb483b1eb6ecad1d54234636d5d04bde19
Martin Hahner <martin.hahner@pm.me>
2022-02-23 11:18:01 +0100

    Initial commit

---
+++ lottery.py
@@ -0,0 +1,55 @@
+import argparse
+
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from tabulate import tabulate
+
+
+
+def print_pairs(seed: int, xls_path: str):
+
+    rng = np.random.default_rng(seed)
+
+    xls = pd.ExcelFile(xls_path)
+
+    first_sheet = xls.parse(0)
+
+    names = list(first_sheet.iloc[3:-1, 0])
+
+    n_participants = len(names)
+
+    assert n_participants % 2 == 0, f'The number of participants is uneven, please find one more participant!'
+
+    n_teams = n_participants // 2
+
+    rng.shuffle(names)
+
+    i = 0
+
+    first = []
+    second = []
+
+    while i < len(names):
+
+        first.append(names[i])
+        second.append(names[i+1])
+
+        i += 2
+
+    d = {'Team Mate A': first, 'Team Mate B': second}
+    df = pd.DataFrame(data=d)
+
+    print(tabulate(df, headers = 'keys', tablefmt = 'psql', showindex=range(1, n_teams +1)))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-p', '--path', type=str,  default=str(Path.home() / 'Downloads' / 'Doodle.xls'), help='seed for random generator')
+    parser.add_argument('-s', '--seed', type=int,  default=42, help='seed for random generator')
+
+    args = parser.parse_args()
+
+    print_pairs(seed=args.seed, xls_path=args.path)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Instructions
+
+1) Download the Doodle survey as Excel file.
+
+2) Change the seed in the command below.
+
+```bash
+git clone https://github.com/MartinHahner/KickerLottery.git
+cd KickerLottery
+conda env create -f environment.yml -y
+conda activate kicker
+python lottery.py --seed 42
+```
+
+3) Enjoy the tournament!
